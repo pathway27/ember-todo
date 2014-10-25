5ff2858fe4cfef6369b501bc28a31b770f10f7f1
S <pathway27@gmail.com>
2014-10-25 00:47:11 +0000

    routes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-window.Todos = Ember.Application.create();
+window.Todos = Ember.Application.create({
+  LOG_TRANSITIONS: true, 
+
+  LOG_TRANSITIONS_INTERNAL: true
+});
 
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
--- js/router.js
+++ js/router.js
@@ -1,9 +1,22 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    this.route('active');
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
